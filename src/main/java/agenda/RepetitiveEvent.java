41a0a79e44972108053dd0aac811d7c8950d8647
Juliengp <145107152+Juliengp@users.noreply.github.com>
2023-11-13 11:28:34 +0100

    Update RepetitiveEvent.java

--- src/main/java/agenda/RepetitiveEvent.java
+++ src/main/java/agenda/RepetitiveEvent.java
@@ -1,5 +1,6 @@
 package agenda;
 
+import java.time.chrono.ChronoLocalDate;
 import java.util.*;
 import java.time.*;
 import java.time.temporal.ChronoUnit;
@@ -8,6 +9,10 @@ import java.time.temporal.ChronoUnit;
  * Description : A repetitive Event
  */
 public class RepetitiveEvent extends Event {
+
+    private ChronoUnit frequency;
+    private ArrayList<LocalDate> exception;
+
     /**
      * Constructs a repetitive event
      *
@@ -23,9 +28,10 @@ public class RepetitiveEvent extends Event {
      */
     public RepetitiveEvent(String title, LocalDateTime start, Duration duration, ChronoUnit frequency) {
         super(title, start, duration);
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        this.frequency=frequency;
+        this.exception= new ArrayList<LocalDate>();
     }
+
 
     /**
      * Adds an exception to the occurrence of this repetitive event
@@ -33,8 +39,11 @@ public class RepetitiveEvent extends Event {
      * @param date the event will not occur at this date
      */
     public void addException(LocalDate date) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        if(exception.contains(date)){
+            throw new IllegalArgumentException("Date déjà présente");
+        }
+        else{
+        this.exception.add(date);}
     }
 
     /**
@@ -42,8 +51,31 @@ public class RepetitiveEvent extends Event {
      * @return the type of repetition
      */
     public ChronoUnit getFrequency() {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");    
+        return this.frequency;
+    }
+
+@Override
+public boolean isInDay(LocalDate aDay){
+    if(exception.contains(aDay)){
+        return false;
+    }
+    LocalDateTime verifDate = this.getStart();
+    int cmpt = 0;
+    while(aDay.plus(cmpt, frequency).compareTo(ChronoLocalDate.from(this.getStart()))>=0){
+        if (super.isInDay(aDay.minus(cmpt, frequency)) && !exception.contains(aDay.minus(cmpt, frequency))) {
+            return true;
+        }
+        cmpt++;
+    }
+    return  false;
+}
+
+@Override
+    public String toString() {
+        return super.toString() +
+                "frequency=" + frequency +
+                ", exceptionLocalDate=" + exception +
+                '}';
     }
 
 }
